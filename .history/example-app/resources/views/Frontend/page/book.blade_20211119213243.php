5faf1f79ffb0bda41fbf2cfa6d14f86a1e3a5587
vanquang202 <83849198+vanquang202@users.noreply.github.com>
2021-11-19 23:07:02 +0700

    update

---
+++ .history/example-app/resources/views/Frontend/page/book.blade_20211119213243.php
@@ -0,0 +1,279 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Book</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css"
+        integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="../css/book.css">
+
+</head>
+
+<body>
+    <div class="container">
+        <div class="main">
+            <div class="wrapper-film-right">
+                <h2>chọn vé/thức ăn</h2>
+                <div class="table-film">
+                    <table id="table-type">
+                        <thead>
+                            <th>Loại vé</th>
+                            <th style="text-align: center;">Số lượng</th>
+                            <th style="text-align: right;">Giá (VNĐ)</th>
+                            <th style="text-align: right;">Tổng (VNĐ)</th>
+                        </thead>
+                        <tbody>
+                            @foreach ($tickets as $ticket)
+
+                                <tr style="width: 350px;">
+                                    <td class="table-type-td">
+                                        <p class="text-bold">{{ $ticket->name }}</p>
+                                    </td>
+                                    <td class="input-type">
+                                        <span class="input-group input-booking">
+
+                                            <button disabled="disabled">
+                                                <span>
+                                                    <i class="fas fa-minus"></i>
+                                                </span>
+
+                                            </button>
+                                        </span>
+                                        <input class="sl" type="number" min="0" value="0" name="s1"
+                                            onchange="updateSL(1)">
+                                        <span class="input-group input-booking">
+
+                                            </button>
+                                        </span>
+                                        {{-- <input class="input1" type="number" min="0" value="0" name="s1"> --}}
+                                        <span class="input-group input-booking">
+
+                                            <button>
+                                                <span>
+                                                    <i class="fas fa-plus"></i>
+                                                </span>
+
+                                            </button>
+                                        </span>
+                                    </td>
+                                    <td style="text-align: right;"> {{ number_format($ticket->unit_price) }}</td>
+                                    <td style="text-align: right;"> 10000</td>
+                                </tr>
+                                {{-- <tr>
+                                <td colspan="3" style="color: #F26B38;">Tổng</td>
+                                <td style="text-align: right;"><label id="tongtien"></label></td>
+                            </tr> --}}
+                            @endforeach
+                        </tbody>
+                    </table>
+                    <table id="table-combo">
+                        <thead>
+                            <th>Combo</th>
+                            <th style="text-align: center;">Số lượng</th>
+                            <th style="text-align: right;">Giá (VNĐ)</th>
+                            <th style="text-align: right;">Tổng (VNĐ)</th>
+                        </thead>
+                        <tbody>
+                            <tr style="text-align: center;">
+                                <td class="table-combo-td">
+                                    <img src="./img/combo-1.jpg" alt="">
+                                    <div class="text">
+                                        <p class="text-bold">Combo 2 Big</p>
+                                        <p class="text-light-combo">1 BẮP + 2 NƯỚC NGỌT 32 OZ</p>
+                                    </div>
+                                </td>
+                                <td class="input-type">
+                                    <span class="input-group input-booking">
+
+                                        </button>
+                                    </span>
+                                </td>
+                                <td style="text-align: right;"><input type="text" readonly class="gia gia1"
+                                        value="65000"></td>
+                                <td style="text-align: right;"><label class="tt tongItem1"></label< /td>
+                            </tr>
+                            {{-- <tr>
+                                <td colspan="3" style="color: #F26B38;">Tổng</td>
+                                <td style="text-align: right;"><label id="tongtien1"></label></td>
+                            </tr> --}}
+                        </tbody>
+                    </table>
+                    <table id="table-combo">
+                        <thead>
+                            <th>Combo</th>
+                            <th style="text-align: center;">Số lượng</th>
+                            <th style="text-align: right;">Giá (VNĐ)</th>
+                            <th style="text-align: right;">Giá (VNĐ)</th>
+                            <th style="text-align: right;">Tổng (VNĐ)</th>
+                        </thead>
+                        <tbody>
+                            <tr style="text-align: center;">
+                                <td class="table-combo-td">
+                                    <img src="./img/combo-1.jpg" alt="">
+                                    <div class="text">
+                                        <p class="text-bold">Combo 2 Big</p>
+                                        <p class="text-light-combo">1 BẮP + 2 NƯỚC NGỌT 32 OZ</p>
+                                    </div>
+                                </td>
+                                <td   class="input-type">
+                                    <span class="input-group input-booking">
+                                        <button disabled="disabled">
+                                            <span>
+                                                <i class="fas fa-minus"></i>
+                                            </span>
+                                        </button>
+                                    </span>
+                                    <input class="sl" type="number" min="0" value="0" name="s1"
+                                        onchange="updateSlcombo(0)">
+                                    <span class="input-group input-booking">
+                                        <button>
+                                            <span>
+                                                <i class="fas fa-plus"></i>
+                                            </span>
+                                        </button>
+                                    </span>
+                                </td>
+                                <td style="text-align: right;"><input type="text" readonly class="gia"
+                                        value="65000"></td>
+                                <td style="text-align: right;"><label class="tt"></label></td>
+                            </tr>
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <div class="main-left">
+            <div class="detail-film">
+                <div class="box-detail">
+                    <div class="img-film">
+                        <img style="height:350px !important" height="100"
+                            src="{{ asset("$URL_IMG_FILM/" . $show_time->film->avatar) }} " alt="">
+                        <p class="text-bold">{{ $show_time->film->name }}</p>
+                        <p class="text-regulary">{{ $show_time->film->cast }}</p>
+                    </div>
+                    <div class="ticket-icon">
+                        <i class="icon-c18"></i>
+                        <span style="color:red;">(*) Phim chỉ dành cho khán giả từ 18 tuổi trở lên</span>
+                    </div>
+                    <div class="ticket-info">
+                        <p><b>Rạp : </b>{{ $show_time->cinema_room->cinema->cluster_cinema->name }} -
+                            {{ $show_time->cinema_room->cinema->name }}</p>
+                        <p><b>Suất chiếu : </b>{{ date('h:i', strtotime($show_time->start_time)) }} | Ngày :
+                            {{ $show_time->show_date }} </p>
+                        {{-- <p><b>Combo : </b>Galaxy Hanoi</p>
+                        <p><b>Ghế : </b>Galaxy Hanoi</p> --}}
+                    </div>
+                    <div class="ticket-price-total">
+                        <p class="total-text">Tổng: <span class="money"><label
+                                    id="tongtien3"></label></span></p>
+                    </div>
+                    <div class="view-more">
+                        <button>Tiếp tục <i class="fas fa-arrow-right"></i></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</body>
+<script>
+    var gia = document.getElementsByClassName("gia");
+    var tt = document.getElementsByClassName("tt");
+    var sl = document.getElementsByClassName("sl");
+
+    function updateSL(i) {
+        tt[i].innerHTML = gia[i].value * sl[i].value;
+        tongtien1();
+    }
+
+    function updateSlcombo(i) {
+        tt[i].innerHTML = gia[i].value * sl[i].value;
+        tongtien2();
+    }
+
+    function tongtien1() {
+        let tongtien = 0;
+        for (j = 0; j < gia.length; j++) {
+            tt[j].innerHTML = gia[j].value * sl[j].value
+            tongtien += Number(tt[j].innerHTML);
+        }
+        document.getElementById("tongtien").innerHTML = tongtien;
+        document.getElementById('tongtien3').innerHTML = Number(document.getElementById("tongtien").innerHTML) + Number(
+            document.getElementById("tongtien2").innerHTML)
+
+    }
+
+    function tongtien2() {
+        let tongtien = 0;
+        for (j = 0; j < gia.length; j++) {
+            tt[j].innerHTML = gia[j].value * sl[j].value
+            tongtien += Number(tt[j].innerHTML);
+        }
+        document.getElementById("tongtien2").innerHTML = tongtien;
+        document.getElementById('tongtien3').innerHTML = Number(document.getElementById("tongtien").innerHTML) + Number(
+            document.getElementById("tongtien2").innerHTML)
+    }
+    window.onload = function() {
+        let tongtiendau = 0;
+        for (i = 0; i < gia.length; i++) {
+            tt[i].innerHTML = gia[i].value * sl[i].value
+            tongtiendau += Number(tt[i].innerHTML);
+        }
+        document.getElementById("tongtien").innerHTML = tongtiendau;
+        document.getElementById('tongtien3').innerHTML = Number(document.getElementById("tongtien").innerHTML) +
+            Number(document.getElementById("tongtien2").innerHTML)
+    }
+</script>
+
+</html>
+
+</body>
+<script>
+    const tongtien1 = document.querySelector('#tongtien1');
+    const tongtien2 = document.querySelector('#tongtien2');
+    const tongtien3 = document.querySelector('#tongtien3');
+    const tongItem1 = document.querySelectorAll('.tongItem1');
+    const tongItem2 = document.querySelectorAll('.tongItem2');
+    const input1 = document.querySelectorAll('.input1');
+    const input2 = document.querySelectorAll('.input2');
+    const gia1 = document.querySelectorAll('.gia1');
+    const gia2 = document.querySelectorAll('.gia2');
+
+    for (let i = 0; i < input1.length; i++) {
+        input1[i].addEventListener('change', () => {
+            tongItem1[i].innerHTML = Number(gia1[i].value) * Number(input1[i].value)
+            tong1()
+        })
+    }
+    for (let i = 0; i < input2.length; i++) {
+        input2[i].addEventListener('change', () => {
+            tongItem2[i].innerHTML = Number(gia2[i].value) * Number(input2[i].value)
+            tong2()
+        })
+    }
+
+    function tong1() {
+        let tong = 0;
+        for (let i = 0; i < tongItem1.length; i++) {
+            tong += Number(tongItem1[i].innerHTML)
+        }
+        tongtien1.innerHTML = tong
+        tongtien3.innerHTML = Number(tongtien1.innerHTML) + Number(tongtien2.innerHTML)
+    }
+
+    function tong2() {
+        let tong = 0;
+        for (let i = 0; i < tongItem2.length; i++) {
+            tong += Number(tongItem2[i].innerHTML)
+        }
+        tongtien2.innerHTML = tong
+        tongtien3.innerHTML = Number(tongtien1.innerHTML) + Number(tongtien2.innerHTML)
+    }
+</script>
+
+</html>
---
+++ .history/example-app/resources/views/Frontend/page/detail_film.blade_20211119155758.php
@@ -0,0 +1,162 @@
+@extends('Frontend.layout_web')
+@section('conten.web')
+
+    <!-- Main Chi Tiết Phim -->
+    <div class="py-8 px-36">
+        <h2 class="py-4">Trang chủ > Phim > <span class="font-bold">{{ $film->name }}</span></h2>
+        <!-- Wrapper -->
+        <div class="">
+            <div class="grid grid-cols-3 gap-10">
+                <!-- Thông Tin Chi Tiết Phim  -->
+                <div class="col-span-2">
+                    <div class="grid grid-cols-3 gap-7 ">
+                        <div class="">
+                            <img src="{{ asset("$URL_IMG_FILM/$film->avatar") }}" alt="">
+                        </div>
+                        <div class="col-span-2 ">
+                            <h1 class="text-3xl">{{ $film->name }}</h1>
+                            <div class="my-3">
+                                <i class="fas fa-star cam text-2xl"></i>
+                                <span class="inline-block mx-3 text-2xl">10.0/10</span>
+                                <a href="#" class="bg-cam text-gray-100 px-2 py-1 mx-2 inline-block text-xl">Đánh
+                                    giá</a>
+                            </div>
+                            <div class="">
+                                <span class="text-white bg-cam inline-block px-1 py-1 text-2xl">C18</span>
+                                <i class="far fa-clock inline-block mx-2 text-2xl"></i>
+                                <span class="text-2xl"> {{ date('H:i:s', strtotime($film->time)) }}</span>
+                            </div>
+                            <div class="my-16">
+                                <span class="text-gray-500 block text-xl">Thể loại:
+                                    <span class="text-black">
+                                        @foreach ($type_films as $type_film)
+                                            @if ($film->film_type_id == $type_film->id_film_type)
+                                                {{ $type_film->name }}
+                                            @endif
+                                        @endforeach
+                                        {{-- {{ $type_film->name }} --}}
+                                    </span>
+                                </span>
+                                <span class="text-gray-500 block text-xl">Quốc gia:
+                                    <span class="text-black">{{ $film->nation }}</span>
+                                </span>
+
+                                <span class="text-gray-500 block text-xl">Nhà SX:
+                                    <span class="text-black">
+                                        {{ $film->producer }}
+                                    </span>
+                                </span>
+                                <span class="text-gray-500 block text-xl">Ngày chiếu:
+                                    <span class="text-black">
+                                        {{ date('d / m / Y ', strtotime($film->premiere_date)) }}
+                                    </span>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- NỘI DUNG PHIM  -->
+                    <div class="">
+                        <span class="text-xl my-5 block font-bold">NỘI DUNG PHIM</span>
+                        <div>{!! $film->summary !!}</div>
+                    </div>
+                    <!-- END NỘI DUNG PHIM -->
+                    <!-- LỊCH CHIẾU  -->
+                    <div class="py-5">
+                        <span class="text-xl my-5 block font-bold">LỊCH CHIẾU</span>
+                        <div class="grid grid-cols-3 gap-5">
+                            <div class="">
+                                <select name="select-ca-nuoc" id="" class="w-full py-2 px-2 border-black border">
+                                    <option value="">Cả Nước</option>
+                                    {{-- @foreach ($clusterCinemas as $clusterCinema)
+                                        <option value="{{ $clusterCinema->id }}">{{ $clusterCinema->name }}</option>
+
+                                    @endforeach --}}
+                                </select>
+                            </div>
+                            <div class="">
+                                <input type="date" class="w-full py-2 px-2 border-black border">
+                            </div>
+                            <div class="">
+                                <select name="select-ca-rap" id="" class="w-full py-2 px-2 border-black border">
+                                    <option value="">Tất Cả Rạp</option>
+                                    @foreach ($cinemaOfFilms as $cinemaOfFilm)
+                                        <option value="{{ $cinemaOfFilm->id }}">{{ $cinemaOfFilm->name }}</option>
+                                    @endforeach
+
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- END LỊCH CHIẾU -->
+                    <!-- CÁC RẠP ĐANG CHIẾU -->
+                    <div class="">
+                        @foreach ($cinemaOfFilms as $cinemaOfFilm)
+                            <div class="py-5">
+                                <span
+                                    class="inline-block bg-camx text-white px-3 py-2">{{ $cinemaOfFilm->name }}</span>
+                                <div class="px-5 py-6 border border-black">
+                                    @foreach ($cinemaRooms as $cinemaRoom)
+                                    @if ($cinemaRoom -> cinema_id == $cinemaOfFilm->id)
+                                        <div class="mb-12">
+                                            <span class="inline-block mr-5"> Phòng : {{ $cinemaRoom -> id_cinema_room }}</span>
+                                            <span
+                                                class="inline-block border border-cam px-2 py-1 mr-3 hover:text-white hover:bg-camx cursor-pointer">
+                                                {{-- {{ date('H:i', strtotime($showtime->start_time)) }} --}}
+                                                {{ date('H:i') }}
+                                            </span>
+                                            <span
+                                                class="inline-block border border-cam px-2 py-1 mr-3 hover:text-white hover:bg-camx cursor-pointer">
+                                                {{ date('H:i ') }}
+                                            </span>
+                                        </div>
+                                    @endif
+                                    @endforeach
+                                </div>
+                            </div>
+                        @endforeach
+                    </div>
+                    <!-- END CÁC RẠP ĐANG CHIẾU -->
+                </div>
+                <!-- END THÔNG TIN CHI TIẾT PHIM  -->
+                <!-- DANH SÁCH PHIM BÊN CẠNH  -->
+                <div class="">
+                    <h2 class="text-xl mt-6">NHẬN KHUYẾN MÃI</h2>
+                    <form action="" class="border-black border p-6 my-8">
+                        <input type="email" placeholder="Email" class="p-2 w-full border mb-3" required>
+                        <input type="submit" class="bg-cam block text-white px-3 py-2 w-full cursor-pointer"
+                            value="Đăng Ký">
+                    </form>
+                    <h2 class="text-xl">PHIM ĐANG CHIẾU</h2>
+                    <div class="my-4">
+                        <div class="group">
+                            <img src="{{ asset('frontend/img/movie1.png') }}" alt="" class="">
+                        </div>
+                        <span class="text-base">THE CONJURING: THE DEVIL MADE ME DO IT</span>
+                        <span class="text-gray-400 block text-sm">THE CONJURING: MA XUI QUỶ KHIẾN
+                        </span>
+                    </div>
+                    <div class="my-4">
+                        <img src="{{ asset('frontend/img/movie1.png') }}" alt="" class="">
+                        <span class="text-base">THE CONJURING: THE DEVIL MADE ME DO IT</span>
+                        <span class="text-gray-400 block text-sm">THE CONJURING: MA XUI QUỶ KHIẾN
+                        </span>
+                    </div>
+                    <div class="my-4">
+                        <img src="{{ asset('frontend/img/movie1.png') }}" alt="" class="">
+                        <span class="text-base">THE CONJURING: THE DEVIL MADE ME DO IT</span>
+                        <span class="text-gray-400 block text-sm">THE CONJURING: MA XUI QUỶ KHIẾN
+                        </span>
+                    </div>
+                    <div class="text-right">
+                        <a href="#"
+                            class="inline-block my-10 cam border px-7 py-4 border-cam hover:text-white hover:bg-camx">XEM
+                            THÊM</a>
+                    </div>
+                </div>
+                <!-- END DANH SÁCH PHIM BÊN CẠNH -->
+            </div>
+        </div>
+        <!-- END Wrapper  -->
+    </div>
+    <!-- END Main Chi Tiết Phim  -->
+@endsection
