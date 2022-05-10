df223855198cb44491a0378912c0077f2723923a
Link <73287503+vubaolinh123@users.noreply.github.com>
2022-05-10 08:55:59 +0700

    ok

--- example-app/resources/views/Backend/include/navbar.blade.php
+++ example-app/resources/views/Backend/include/navbar.blade.php
@@ -145,32 +145,7 @@
 </li>
 
 
-  <li class="nav-label">Apps</li>
-<li><a class="has-arrow" href="javascript:void()" aria-expanded="false"><i class="icon icon-app-store"></i><span
-            class="nav-text">Apps</span></a>
-    <ul aria-expanded="false">
-        <li><a href="./app-profile.html">Profile</a></li>
-        <li><a class="has-arrow" href="javascript:void()" aria-expanded="false">Email</a>
-            <ul aria-expanded="false">
-                <li><a href="./email-compose.html">Compose</a></li>
-                <li><a href="./email-inbox.html">Inbox</a></li>
-                <li><a href="./email-read.html">Read</a></li>
-            </ul>
-        </li>
-        <li><a href="./app-calender.html">Calendar</a></li>
-    </ul>
-</li>
-<li><a class="has-arrow" href="javascript:void()" aria-expanded="false"><i class="icon icon-chart-bar-33"></i><span
-            class="nav-text">Charts</span></a>
-    <ul aria-expanded="false">
-        <li><a href="./chart-flot.html">Flot</a></li>
-        <li><a href="./chart-morris.html">Morris</a></li>
-        <li><a href="./chart-chartjs.html">Chartjs</a></li>
-        <li><a href="./chart-chartist.html">Chartist</a></li>
-        <li><a href="./chart-sparkline.html">Sparkline</a></li>
-        <li><a href="./chart-peity.html">Peity</a></li>
-    </ul>
-</li>
+
 @hasAdmin('admin')
 <li>
     <a class="has-arrow" href="{{ route('admin.manage.user') }}" >
